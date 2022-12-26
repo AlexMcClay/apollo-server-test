3af98417549c1c33bb243742f2ec12c61aa584e8
AlexMcClay <smurf.alix@gmail.com>
2022-12-26 12:00:08 +1030

    added postgraphile and apollo client

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,10 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { resolvers, typeDefs } from '../../graphql';
+// import { ApolloServer } from '@apollo/server';
+// import { startServerAndCreateNextHandler } from '@as-integrations/next';
+// import { resolvers, typeDefs } from '../../graphql';
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-});
+// const server = new ApolloServer({
+//   resolvers,
+//   typeDefs,
+// });
 
-export default startServerAndCreateNextHandler(server);
+// export default startServerAndCreateNextHandler(server);
